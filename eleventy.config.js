4ea1d2de28d0df4bbed0cf29607b07187170b0cd
pinkkoala95 <emma.keeley@madetech.com>
2024-07-19 11:20:25 +0100

    update eleventy config

--- eleventy.config.js
+++ eleventy.config.js
@@ -30,6 +30,6 @@ module.exports = function (eleventyConfig) {
       layouts: "_layouts",
       includes: "_components",
     },
-    pathPrefix: process.env.GITHUB_ACTIONS && "/govuk-design-history-template/",
+    pathPrefix: process.env.GITHUB_ACTIONS && "/stl-design-history/",
   };
 };
